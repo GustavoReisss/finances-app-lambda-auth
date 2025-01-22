18b1c0fc5f49dd4251f4852bb54b738cde30c435
Gustavo Reis <gustavo.reis20000@gmail.com>
2025-01-21 21:58:18 -0300

    updating error message

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -4,10 +4,14 @@ from aws_lambda_powertools.event_handler.api_gateway import Router
 from services.auth_service import AuthService
 from utils.parse_cookies import parse_cookies
 
-cors_config = CORSConfig(allow_credentials=True, expose_headers=["Set-Cookie"], allow_headers=["Set-Cookie"])
+cors_config = CORSConfig(
+    allow_credentials=True, expose_headers=["Set-Cookie"], allow_headers=["Set-Cookie"]
+)
+
 router = Router()
 
 __auth_service: AuthService = None
+
 
 def __setup_services():
     global __auth_service
@@ -26,6 +30,7 @@ def login():
 
     return __auth_service.login(user_id, password)
 
+
 @router.post("/register")
 def register():
     body = resolver.current_event.body
@@ -34,15 +39,16 @@ def register():
 
 @router.post("/logout")
 def logout():
-    cookies = resolver.current_event.get_header_value('Cookie')
+    cookies = resolver.current_event.get_header_value("Cookie")
     cookies_dict = parse_cookies(cookies)
     return __auth_service.logout(cookies_dict)
-    
+
 
 resolver = APIGatewayHttpResolver(cors=cors_config)
 resolver.include_router(router=router, prefix="")
 
-def lambda_handler(event, context = None):
+
+def lambda_handler(event, context=None):
     __setup_services()
 
     if "body" in event and type(event["body"]) is str:
--- infra/lambda_function_url.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_lambda_function_url" "lambda_url" {
-  function_name      = aws_lambda_function.lambda.function_name
-  authorization_type = "NONE"
-}
